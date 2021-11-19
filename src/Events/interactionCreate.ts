dc591c40c2b6986f062f5e07af490d2ca2123551
RedstoneZockt <45714458+RedstoneZockt@users.noreply.github.com>
2021-11-19 17:12:15 +0100

    versuche

--- src/Events/interactionCreate.ts
+++ src/Events/interactionCreate.ts
@@ -1,12 +1,23 @@
 import { Event } from "../Interfaces";
 import Client from "../Client";
-import { Interaction } from "discord.js";
+import { Interaction, MessageActionRow, MessageButton } from "discord.js";
 export const event: Event = {
     name: 'interactionCreate',
-    run: (client: Client, interaction: Interaction) => {
+    run: async (client: Client, interaction: Interaction) => {
         console.log(`Interaction created: ${interaction.id}`);
         if(interaction.isCommand()) {
             console.log(`Command: ${interaction.commandName}`);
+            if(interaction.commandName === 'test') {
+                const data = new MessageActionRow().addComponents(new MessageButton().setLabel('Test').setCustomId('test').setStyle('PRIMARY'));
+                await interaction.reply({content: "Test", components: [data]});
+            }
+        }else if(interaction.isContextMenu()) {
+            console.log(`Context Menu: ${interaction.commandName}`);
+        }else if(interaction.isButton()) {
+            console.log(`Button: ${interaction}`);
+            console.log(interaction.component)
+            let button: MessageButton = interaction.component as MessageButton;
+            interaction.reply({content: "Button mit Label: `" + button.label + "` gedrückt"});
         }
     }
 }
--- src/Events/ready.ts
+++ src/Events/ready.ts
@@ -1,10 +1,21 @@
 import { Event } from '../Interfaces';
 import Client from '../Client';
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { Options } from 'discord.js';
+
 
 export const event: Event = {
     name: 'ready',
     run: (client: Client, ...args) => {
         console.log(`Logged in as ${client.user.tag}!`);
+        console.log(`Loading SlashCommands`)
+        for (const guild of client.guilds.cache.values()) {
+            const slash_command = new SlashCommandBuilder()
+            .setName("create_embed")
+            .setDescription("Creates an embed")
+            .addIntegerOption((option) => option.setName("ammount").setDescription("Anzahl der Embeds").setRequired(true).addChoice("1", 1).addChoice("2", 2).addChoice("3", 3).addChoice("4", 4).addChoice("5", 5).addChoice("6", 6).addChoice("7", 7).addChoice("8", 8).addChoice("9", 9).addChoice("10", 10));
+            guild.commands.create(slash_command.toJSON());
+        }
     }
 }
 
